0f817ac0d02b787f290af0542a4696457e621ac0
Justin <justin@baal-software.com>
2019-04-03 15:03:10 -0600

    Add ability for exhibit to return hash of exhibits with patrons as value

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,9 +1,10 @@
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -12,5 +13,18 @@ class Museum
 
   def recommend_exhibits(patron)
     @exhibits.find_all { |exhibit| patron.interests.include? exhibit.name }
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    @exhibits.reduce({}) do |result, exhibit|
+      result[exhibit] = @patrons.find_all do |patron|
+        patron.interests.include? exhibit.name
+      end
+      result
+    end
   end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -4,13 +4,30 @@ require './lib/museum'
 require './lib/exhibit'
 
 class MuseumTest < Minitest::Test
-  attr_reader :dmns
+  attr_reader :dmns, :bob, :sally,
+              :gems_and_minerals, :dead_sea_scrolls, :imax
 
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    @imax = Exhibit.new("IMAX", 15)
+    @bob = Patron.new("Bob", 20)
+    @sally = Patron.new("Sally", 20)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Gems and Minerals")
+    sally.add_interest("IMAX")
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    dmns.admit(bob)
+    dmns.admit(sally)
   end
 
   def test_it_exist
+    dmns = Museum.new("Denver Museum of Nature and Science")
     assert_instance_of Museum, dmns
   end
 
@@ -19,40 +36,31 @@ class MuseumTest < Minitest::Test
   end
 
   def test_it_has_no_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
     assert_equal [], dmns.exhibits
   end
 
+  def test_it_has_no_patrons
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    assert_equal [], dmns.patrons
+  end
+
   def test_it_has_exhibits
-    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
-    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
-    imax = Exhibit.new("IMAX", 15)
-
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
-
     assert_equal [gems_and_minerals,dead_sea_scrolls,imax], dmns.exhibits
   end
 
   def test_it_can_reccomend_exhibits
-    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
-    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
-    imax = Exhibit.new("IMAX", 15)
-
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
-
-    bob = Patron.new("Bob", 20)
-    bob.add_interest("Dead Sea Scrolls")
-    bob.add_interest("Gems and Minerals")
-    sally = Patron.new("Sally", 20)
-    sally.add_interest("IMAX")
-
     assert_equal [gems_and_minerals, dead_sea_scrolls],dmns.recommend_exhibits(bob)
     assert_equal [imax], dmns.recommend_exhibits(sally)
-
   end
 
+  def test_it_can_admit_patrons
+    assert_equal [bob, sally], dmns.patrons
+  end
+
+  def test_it_can_show_patrons_by_exhibit_interest
+    expected = {gems_and_minerals=>[bob], dead_sea_scrolls=>[bob], imax=>[sally]}
+    assert_equal expected, dmns.patrons_by_exhibit_interest
+  end
 
 end
